9515975acdb9332d0dd217fb163f45433b999b7a
liwei <liweilijie@gmail.com>
2024-05-31 00:20:51 +0800

    add CommonResp class to response

--- java/springboot/wiki/src/main/java/au/liw/wiki/controller/EbookController.java
+++ java/springboot/wiki/src/main/java/au/liw/wiki/controller/EbookController.java
@@ -1,6 +1,7 @@
 package au.liw.wiki.controller;
 
 import au.liw.wiki.domain.Ebook;
+import au.liw.wiki.resp.CommonResp;
 import au.liw.wiki.service.EbookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,7 +19,11 @@ public class EbookController {
     private EbookService ebookService;
 
     @GetMapping("/list")
-    public List<Ebook> list() {
-        return ebookService.list();
+    public CommonResp list() {
+
+        CommonResp<List<Ebook>> resp = new CommonResp<>();
+        List<Ebook> list = ebookService.list();
+        resp.setContent(list);
+        return resp;
     }
 }
