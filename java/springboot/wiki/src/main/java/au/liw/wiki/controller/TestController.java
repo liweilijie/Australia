24868cd69d9e368990376323a0afcab3727cb34e
liwei <liweilijie@gmail.com>
2024-05-30 22:12:57 +0800

    add the read variable from properties.

--- java/springboot/wiki/src/main/java/au/liw/wiki/controller/TestController.java
+++ java/springboot/wiki/src/main/java/au/liw/wiki/controller/TestController.java
@@ -1,13 +1,18 @@
 package au.liw.wiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
+
+    @Value("${test.hello:default}")
+    private String testHello;
+
     @RequestMapping("/hello")
     public String hello() {
-        return "Hello World";
+        return "Hello World: " + testHello;
     }
 }
