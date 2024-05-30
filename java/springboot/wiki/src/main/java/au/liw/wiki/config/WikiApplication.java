148790f581135d46eaad07625424bb19c1591b47
liwei <liweilijie@gmail.com>
2024-05-30 21:58:56 +0800

    add /hello api and move Application to config package.

--- java/springboot/wiki/src/main/java/au/liw/wiki/config/WikiApplication.java
+++ java/springboot/wiki/src/main/java/au/liw/wiki/config/WikiApplication.java
@@ -1,11 +1,13 @@
-package au.liw.wiki;
+package au.liw.wiki.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 
+@ComponentScan("au.liw") //@ComponentScan({"au.liw", "com.text"})
 @SpringBootApplication
 public class WikiApplication {
 
