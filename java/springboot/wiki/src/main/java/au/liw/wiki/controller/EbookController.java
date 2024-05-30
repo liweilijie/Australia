53bd498af47d35604f33ddf2cbb4bbe51f8be6c9
liwei <liweilijie@gmail.com>
2024-05-31 00:13:11 +0800

    add ebook api /ebook/list to query all data.

---
+++ java/springboot/wiki/src/main/java/au/liw/wiki/controller/EbookController.java
@@ -0,0 +1,24 @@
+package au.liw.wiki.controller;
+
+import au.liw.wiki.domain.Ebook;
+import au.liw.wiki.service.EbookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/ebook")
+public class EbookController {
+
+
+    @Autowired
+    private EbookService ebookService;
+
+    @GetMapping("/list")
+    public List<Ebook> list() {
+        return ebookService.list();
+    }
+}
---
+++ java/springboot/wiki/src/main/java/au/liw/wiki/service/EbookService.java
@@ -0,0 +1,19 @@
+package au.liw.wiki.service;
+
+import au.liw.wiki.domain.Ebook;
+import au.liw.wiki.mapper.EbookMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EbookService {
+
+    @Autowired
+    private EbookMapper ebookMapper;
+
+    public List<Ebook> list() {
+        return ebookMapper.selectByExample(null);
+    }
+}
