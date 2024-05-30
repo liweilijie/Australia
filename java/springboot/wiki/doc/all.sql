7b39ab3af8985fe84d0837ff16ca04d24aa607eb
liwei <liweilijie@gmail.com>
2024-05-30 23:59:34 +0800

    add mybatis-generator code and generator demo files.

--- java/springboot/wiki/doc/all.sql
+++ java/springboot/wiki/doc/all.sql
@@ -8,3 +8,13 @@ create table `test` (
 
 insert into `test`(id, name, password) values (1, 'test', '123456');
 insert into `test`(id, name, password) values (2, 'Australia', 'merben');
+
+drop table if exists `demo`;
+create table `demo` (
+  `id` bigint not null comment 'id',
+  `name` varchar(50) comment 'name',
+  primary key (`id`)
+) engine=innodb default charset=utf8mb4 comment='just for demo test';
+
+insert into `demo`(id, name) values (1, 'demo');
+insert into `demo`(id, name) values (2, 'Australia');
