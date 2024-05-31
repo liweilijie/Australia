ea2b720bd02fd1d2b13655c94370abdbb2ed2b38
liwei <liweilijie@gmail.com>
2024-05-31 13:42:23 +0800

    initial auwikiweb and add layout.

---
+++ vue/README.md
@@ -0,0 +1,6 @@
+# vue
+
+```bash
+# setting taobao registry
+npm config set registry https://registry.npmmirror.com
+```
---
+++ vue/auwikiweb/src/views/AboutView.vue
@@ -0,0 +1,13 @@
+import 'ant-design-vue/dist/reset.css';
+
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <a-button type="primary" shape="round" :size="size">
+      <template #icon>
+        <DownloadOutlined />
+      </template>
+      Download
+    </a-button>
+  </div>
+</template>
