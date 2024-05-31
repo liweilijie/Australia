5102fec3afb0a92862bb03acb50e101662a58377
liwei <liweilijie@gmail.com>
2024-05-31 10:30:24 +0800

    add util.CopyUtil class.

--- java/springboot/wiki/src/main/java/au/liw/wiki/service/EbookService.java
+++ java/springboot/wiki/src/main/java/au/liw/wiki/service/EbookService.java
@@ -5,11 +5,10 @@ import au.liw.wiki.domain.EbookExample;
 import au.liw.wiki.mapper.EbookMapper;
 import au.liw.wiki.req.EbookReq;
 import au.liw.wiki.resp.EbookResp;
-import org.springframework.beans.BeanUtils;
+import au.liw.wiki.util.CopyUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -24,13 +23,9 @@ public class EbookService {
         criteria.andNameLike("%" + req.getName() + "%");
         List<Ebook> ebooks = ebookMapper.selectByExample(ebookExample);
 
-        List<EbookResp> respList = new ArrayList<>();
-        for (Ebook ebook : ebooks) {
-            EbookResp ebookResp = new EbookResp();
-            BeanUtils.copyProperties(ebook, ebookResp);
-//            ebookResp.setId(123L);
-            respList.add(ebookResp);
-        }
+
+        List<EbookResp> respList = CopyUtil.copyList(ebooks, EbookResp.class);
+
         return respList;
     }
 }
