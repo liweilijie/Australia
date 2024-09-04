efae0a2353d4e51a56fcc3ee374601cfcc283bac
liwei <liweilijie@gmail.com>
2024-09-04 13:49:11 +0800

    mybatis first demo.

---
+++ java/mybatis/mybatis-demo/src/main/java/com/liw/pojo/User.java
@@ -0,0 +1,61 @@
+package com.liw.pojo;
+
+public class User {
+    private Integer id;
+    private String username;
+    private String password;
+    private String gender;
+    private String addr;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getAddr() {
+        return addr;
+    }
+
+    public void setAddr(String addr) {
+        this.addr = addr;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("User{");
+        sb.append("id=").append(id);
+        sb.append(", username='").append(username).append('\'');
+        sb.append(", password='").append(password).append('\'');
+        sb.append(", gender='").append(gender).append('\'');
+        sb.append(", addr='").append(addr).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+}
